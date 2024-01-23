abcf34772f0e6f03cbcb4b0943870ca99ba107f6
Soutikmondal <69642549+Soutikmondal@users.noreply.github.com>
2024-01-23 11:06:25 +0530

    Home_Page

--- Blood_Bank_Management_System/Home_Page/login_process.php
+++ Blood_Bank_Management_System/Home_Page/login_process.php
@@ -22,16 +22,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if ($result) {
         // Login successful
-        session_start();
-        $_SESSION['RID'] = $result['RID']; 
-        $_SESSION['username'] = $username;
-        header("Location: http://localhost/Blood_Bank_Management_System/Home_Page.html"); // Redirect to the home page
-        exit();
+        header('Content-Type: application/json');
+        echo json_encode(array('success' => $success));
     }
 }
 
 // If login fails or if the request is not POST, redirect back to the login page with an error parameter
-header("Location: login.html?error=1");
+header("Location: Register_Page.html?error=1");
 exit();
 
 // Add your database connection closing code here
